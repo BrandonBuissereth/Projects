9dd2082dc38e91d13be0e71e815d1485d8b0932d
BrandonBuissereth <BrandonJBuissereth@gmail.com>
2026-09-03 01:25:02 -0400

    Add clearance rate query for agency types

--- NJ-2024-Crime-Database/queries.sql
+++ NJ-2024-Crime-Database/queries.sql
@@ -21,3 +21,31 @@ LEFT JOIN INCLUDES inc
 GROUP BY a.county
 ORDER BY violent_ratio DESC
 LIMIT 50;
+
+-- Query 2
+SELECT
+    a.agency_type_name,
+    COUNT(i.incident_id) AS total_incidents,
+    SUM(CASE
+        WHEN i.cleared_flag IS NOT NULL
+        AND i.cleared_flag != ''
+        AND i.cleared_flag != '6'
+        THEN 1
+        ELSE 0
+    END) AS cleared_incidents,
+    ROUND(
+        100.0 * SUM(CASE
+            WHEN i.cleared_flag IS NOT NULL
+            AND i.cleared_flag != ''
+            AND i.cleared_flag != '6'
+            THEN 1
+            ELSE 0
+        END) / COUNT(i.incident_id),
+        2
+    ) AS clearance_rate
+FROM AGENCY a
+JOIN INCIDENT i
+    ON a.agency_id = i.agency_id
+GROUP BY a.agency_type_name
+ORDER BY clearance_rate DESC
+LIMIT 50;
